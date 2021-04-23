f23da1a796488c7de2fa729a8179b2d9ca6d95ee
TaniaY21 <taniay@hotmail.co.uk>
2021-04-23 16:40:38 +0100

    final version

--- README.md
+++ README.md
@@ -1,1 +1,83 @@
-# Rock-Paper-Scissors
+# Things I learned:
+
+## `If` VS `switch` statements
+
+Originally I had written the logic for keeping score as a long list of `if` statements:
+
+```javascript
+function compareMoves() {
+  if (computerMove === humanMove) {
+    game_update.innerText = `That's a tie! Bot picked ${computerMove}`;
+  }
+  if (humanMove === "rock") {
+    if (computerMove === "scissors") {
+      humanScore++;
+      game_update.innerText = `You win! Bot picked ${computerMove}`;
+    } else if (computerMove === "paper") {
+      computerScore++;
+      game_update.innerText = `You lose! Bot picked ${computerMove}`;
+    }
+  }
+  if (humanMove === "paper") {
+    if (computerMove === "rock") {
+      humanScore++;
+    } else if (computerMove === "scissors") {
+      computerScore++;
+    }
+  }
+
+  if (humanMove === "scissors") {
+    if (computerMove === "paper") {
+      humanScore++;
+    } else if (computerMove === "rock") {
+      computerScore++;
+    }
+  }
+}
+```
+
+After looking at some code online and another tutorial, I realised that in this case it would be better to use `switch` statements instead. What was **REALLY** interesting was that the switch statement should take **TWO EXPRESSIONS** and have the value for each expression combined in each case.... crazy! 🤯
+
+```` javascript
+function compareMoves(userInput) {
+  let computerMove = generateComputerMove();
+  let humanMove = userInput;
+
+  switch (userInput + computerMove) {
+    case "rockscissors":   // this was so cool!
+    case "paperrock":
+    case "scissorspaper":
+      userWins(userInput, computerMove);
+      break;
+````
+
+
+
+## Better way of generating computer move
+
+Initially I just generated the range of numbers, and then went through three `if` statements in order to assign a value of rock, paper or scissors to each random number.
+
+```javascript
+function generateComputerMove() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  if (number === 0) {
+    value = "rock";
+  } else if (number === 1) {
+    value = "paper";
+  } else {
+    value = "scissors";
+  }
+  return value;
+}
+```
+
+I am not sure what this type of syntax is called, but after looking online I realised I can put the `randomNumber` in brackets `[]` instead. Still not sure how it actually works but it works and makes the code shorter:
+
+```javascript
+function generateComputerMove() {
+  let moves = ["rock", "paper", "scissors"];
+  let randomNumber = Math.floor(Math.random() * 3);
+  return moves[randomNumber];
+}
+```
+Overall I learned a lot, just wish I wasn't so stressed out over it!
--- main.js
+++ main.js
@@ -12,27 +12,33 @@ const bot_Scoreboard = document.getElementById("computerScore");
 const game_update = document.getElementById("game-update");
 
 function userWins(human, computer) {
-    humanScore++;
-    you_Scoreboard.innerHTML = humanScore;
-    game_update.innerText = `You win! Bot picked ${computer}`;
-    
-    document.getElementById(human).classList.add('green-glow');
-    setTimeout(function() { document.getElementById(human).classList.remove('green-glow')}, 500);
+  humanScore++;
+  you_Scoreboard.innerHTML = humanScore;
+  game_update.innerText = `You win! Bot picked ${computer}`;
+
+  document.getElementById(human).classList.add('green-glow');
+  setTimeout(function () {
+    document.getElementById(human).classList.remove('green-glow')
+  }, 500);
 };
 
 function userLoses(human, computer) {
-    game_update.innerText = `You lose! Bot picked ${computer}`;
-    computerScore++;
-    bot_Scoreboard.innerHTML = computerScore;
-        
-    document.getElementById(human).classList.add('red-glow');
-    setTimeout(function() { document.getElementById(human).classList.remove('red-glow')}, 500);
+  game_update.innerText = `You lose! Bot picked ${computer}`;
+  computerScore++;
+  bot_Scoreboard.innerHTML = computerScore;
+
+  document.getElementById(human).classList.add('red-glow');
+  setTimeout(function () {
+    document.getElementById(human).classList.remove('red-glow')
+  }, 500);
 };
 
 function tie(human, computer) {
-    document.getElementById(human).classList.add('black-glow');
-    setTimeout(function() { document.getElementById(human).classList.remove('black-glow')}, 500);
-    game_update.innerText = `It's a tie! Bot picked ${computer}`;
+  document.getElementById(human).classList.add('black-glow');
+  setTimeout(function () {
+    document.getElementById(human).classList.remove('black-glow')
+  }, 500);
+  game_update.innerText = `It's a tie! Bot picked ${computer}`;
 };
 
 
@@ -52,16 +58,7 @@ scissor_img.addEventListener("click", function () {
 function generateComputerMove() {
   let randomNumber = Math.floor(Math.random() * 3); // number between 0 and 2
   let moves = ["rock", "paper", "scissors"];
-  return moves[randomNumber]; // found this online: replaced if statements
-
-  // if (number === 0) {
-  //     value = 'rock';
-  // } else if (number === 1) {
-  //     value = 'paper';
-  // } else {
-  //     value = 'scissors';
-  // }
-  // return value
+  return moves[randomNumber];
 }
 
 function playGame() {
@@ -86,65 +83,12 @@ function compareMoves(userInput) {
     case "scissorsrock":
     case "rockpaper":
     case "paperscissors":
-        userLoses(userInput, computerMove);
+      userLoses(userInput, computerMove);
       break;
     case "rockrock":
     case "paperpaper":
     case "scissorsscissors":
-tie(userInput, computerMove);
+      tie(userInput, computerMove);
       break;
   }
-}
-
-
-// old way: better to use switch statement
-
-// if (computerMove === humanMove) {
-//     game_update.innerText = `That's a tie! Bot picked ${computerMove}`;
-// };
-
-// if (humanMove === 'rock') {
-//     if (computerMove === 'scissors') {
-//         humanScore++;
-//         computerScore--;
-//         game_update.innerText = `You win! Bot picked ${computerMove}`
-//     } else if (computerMove === 'paper') {
-//         computerScore++;
-//         humanScore--;
-//         game_update.innerText = `You lose! Bot picked ${computerMove}`
-//     }
-// };
-//     if (humanMove === 'paper') {
-//         if (computerMove === 'rock') {
-//             humanScore++;
-//             computerScore--;
-//             alert(`You win! Your score: ${humanScore}. Computer score: ${computerScore}`)
-//         } else if (computerMove === 'scissors') {
-//             computerScore++;
-//             humanScore--;
-//             alert(`You lose! Your score: ${humanScore}. Computer score: ${computerScore}`)
-//         }
-//     };
-
-//     if (humanMove === 'scissors') {
-//         if (computerMove === 'paper') {
-//             humanScore++;
-
-//             alert(`You win! Your score: ${humanScore}. Computer score: ${computerScore}`)
-//         } else if (computerMove === 'rock') {
-//             // computerScore++;
-//             humanScore--;
-//             alert(`You lose! Your score: ${humanScore}. Computer score: ${computerScore}`)
-//         }
-//     };
-
-//     if (humanMove === "quit") {
-//         isgameover = true;
-//         console.log(`isgameover = ${isgameover}`)
-//         if (computerScore > humanScore) {
-//             alert(`The winner is the mighty AI. Your score: ${humanScore}, Computer score: ${computerScore}`)
-//         } else {
-//             alert(`The winner is you! Your score: ${humanScore}, Computer score: ${computerScore}`)
-//         }
-//     };
-
+};
